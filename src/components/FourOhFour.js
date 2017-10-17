7fa675457877da6aead3a8932fe710668725cd06
Roman Levitas <rlevitas@rbmtechnologies.com>
2017-10-16 20:30:10 -0400

    404 & edit/delete on show page

---
+++ src/components/FourOhFour.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FourOhFour = props => {
+
+  return(
+    <div>
+      <h1>404: Page Not Found</h1>
+      <div>or data has been deleted</div>
+    </div>
+  )
+}
+
+export default FourOhFour;
